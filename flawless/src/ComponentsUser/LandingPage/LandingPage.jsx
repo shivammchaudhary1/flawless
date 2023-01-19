586e6e7a52a42ff75a2c90f97689e140004814bb
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-01-19 20:46:01 +0530

    afternoon update

---
+++ flawless/src/ComponentsUser/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Carousal from "./Carousal";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Carousal />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ flawless/src/ComponentsUser/OrderingPage/OrderingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderingPage = () => {
+  return <div></div>;
+};
+
+export default OrderingPage;
