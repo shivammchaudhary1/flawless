e74d5fff0a0b59ba7bbab31f67d421fe9d4a8754
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-01-20 03:10:36 +0530

    day_3

---
+++ flawless/src/ComponentsUser/LandingPage/AfterCarousal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Image, Stack, Box, Center } from "@chakra-ui/react";
+
+const AfterCarousal = () => {
+  return (
+    <>
+      <Center>
+        <Stack direction="row">
+          <Image
+            objectFit="cover"
+            src="https://images-static.nykaa.com/uploads/fad4269e-a68e-4eca-9244-da27880090c7.jpg?tr=w-600,cm-pad_resize"
+            alt="Dan Abramov"
+          />
+          <Image
+            objectFit="cover"
+            src="https://images-static.nykaa.com/uploads/5e9e60a2-2c21-48b9-98b8-5ba9155ddbe5.jpg?tr=w-600,cm-pad_resize"
+            alt="Dan Abramov"
+          />
+        </Stack>
+      </Center>
+
+      <Center p={10}>
+        <Image
+          src="https://images-static.nykaa.com/uploads/2030f573-1d09-432d-aacb-24672ba9a0e1.jpg?tr=w-1200,cm-pad_resize"
+          alt="Image"
+        />
+      </Center>
+    </>
+  );
+};
+
+export default AfterCarousal;
---
+++ flawless/src/ComponentsUser/LandingPage/OnlyatNykaa.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Box, Image, Grid, Center, HStack } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+
+const getData = () => {
+  return axios.get(`http://localhost:8080/onlyatnykaa`);
+};
+
+const OnlyatNykaa = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData().then((res) => {
+      setData(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <Box p={5}>
+        <Text fontSize={25} textAlign={["left", "center"]} fontWeight="bold">
+          Top Brands
+        </Text>
+      </Box>
+
+      <Center>
+        <Grid templateColumns="repeat(3, 1fr)" gap={2}>
+          {data.map((item) => {
+            return (
+              <Box
+                maxW="sm"
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+                key={item.id}
+              >
+                <Image
+                  src={item.brand_image}
+                  alt={item.brand_product_description}
+                />
+
+                <Box p="6">
+                  <Box
+                    mt="1"
+                    fontWeight="bold"
+                    as="h4"
+                    lineHeight="tight"
+                    noOfLines={1}
+                    color="#E80071"
+                  >
+                    {item.brand_product_name}
+                  </Box>
+
+                  <Box
+                    mt="1"
+                    fontWeight="semibold"
+                    as="h4"
+                    lineHeight="tight"
+                    noOfLines={1}
+                  >
+                    {item.brand_product_description}
+                  </Box>
+                </Box>
+              </Box>
+            );
+          })}
+        </Grid>
+      </Center>
+    </>
+  );
+};
+
+export default OnlyatNykaa;
