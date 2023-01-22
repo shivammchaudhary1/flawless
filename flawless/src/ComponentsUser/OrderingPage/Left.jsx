e99d777f70758f2f45ca7df75820e80b2752a2ff
shivammchaudhary1 <shivamchaudhary75@gmail.com>
2023-01-22 18:10:47 +0530

    product page done with functionality

--- flawless/src/ComponentsUser/OrderingPage/Left.jsx
+++ flawless/src/ComponentsUser/OrderingPage/Left.jsx
@@ -1,103 +1,154 @@
-import {
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuItemOption,
-  MenuGroup,
-  MenuOptionGroup,
-  MenuDivider,
-  Button,
-  Box,
-  VStack,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
-import { Grid, GridItem } from "@chakra-ui/react";
-
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Image,
+  Badge,
+  SimpleGrid,
+  Grid,
+  Button,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
+
+import { StarIcon } from "@chakra-ui/icons";
 
 const getData = () => {
   return axios.get(`http://localhost:8080/eye`);
 };
 
 const Left = () => {
-  return (
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setLoading(true);
+    getData()
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+  console.log(data);
+
+  return loading ? (
+    <h1>...Loding</h1>
+  ) : error ? (
+    <h1>Something Went Wrong</h1>
+  ) : (
     <>
-      <Box display="flex">
-        <Box m={25} p={25}>
-          <Box mb={5}>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Sorting By Popularity
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-          <Box>
-            <Menu>
-              <MenuButton pl={5} as={Button} rightIcon={<ChevronDownIcon />}>
-                Category
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-          <Box>
-            <Menu>
-              <MenuButton pl={10} as={Button} rightIcon={<ChevronDownIcon />}>
-                Brand
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-          <Box>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Price
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-          <Box>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                Discount
-              </MenuButton>
-              <MenuList>
-                <MenuItem>Download</MenuItem>
-                <MenuItem>Create a Copy</MenuItem>
-                <MenuItem>Mark as Draft</MenuItem>
-                <MenuItem>Delete</MenuItem>
-                <MenuItem>Attend a Workshop</MenuItem>
-              </MenuList>
-            </Menu>
-          </Box>
-        </Box>
-        <Box>R</Box>
+      {/* Card  */}
+
+      <Box w="80%" m="auto">
+        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+          {data.map((property) => (
+            // <p>{e.product_giftcard}</p>
+
+            <Box
+              align={"center"}
+              maxW="sm"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+            >
+              <Box _hover={{ boxSize: "200px" }}>
+                <Image
+                  src={property.product_image}
+                  alt={property.product_name}
+                />
+              </Box>
+
+              <Box p="6">
+                <Box display="flex" alignItems="baseline">
+                  <Badge borderRadius="full" px="2" colorScheme="teal">
+                    {property.product_off}
+                  </Badge>
+                </Box>
+
+                <Box
+                  mt="1"
+                  fontWeight="semibold"
+                  as="h3"
+                  lineHeight="tight"
+                  noOfLines={3}
+                  align={"left"}
+                >
+                  {property.product_name}
+                </Box>
+
+                <Box>
+                  <Box
+                    as="span"
+                    color="gray.600"
+                    fontSize="sm"
+                    fontWeight="semibold"
+                  >
+                    MRP : {property.product_price}
+                  </Box>
+                  <Text textDecoration={"line-through"} color={"gray.600"}>
+                    {property.product_mrp}
+                  </Text>
+                </Box>
+                <Box fontSize="sm" color="#FD7091">
+                  {property.product_giftcard}
+                </Box>
+
+                <Box display="flex" mt="2" alignItems="center">
+                  {Array(5)
+                    .fill("")
+                    .map((_, i) => (
+                      <StarIcon
+                        align={"center"}
+                        key={i}
+                        color={i < property.rating ? "teal.500" : "gray.300"}
+                      />
+                    ))}
+                  <Box
+                    as="span"
+                    ml="2"
+                    color="gray.600"
+                    fontSize="sm"
+                    align={"center"}
+                  >
+                    {property.product_rating} reviews
+                  </Box>
+                  <Button
+                    bg="#EA3891"
+                    color="#FFFFFF"
+                    _hover={{ bg: "#CE0A6C" }}
+                  >
+                    Add to cart
+                  </Button>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </Box>
+
+      {/* Pagination  */}
+
+      <Box>
+        <Flex
+          bg="#edf3f8"
+          _dark={{ bg: "#3e3e3e" }}
+          p={50}
+          alignItems="center"
+          justifyContent="center"
+          w="full"
+        >
+          <Flex>
+            <Button>previous</Button>
+            <Button>{page}</Button>
+            <Button>Next</Button>
+          </Flex>
+        </Flex>
       </Box>
     </>
   );
